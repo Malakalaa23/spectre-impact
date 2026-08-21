6b1dab5878b9cc38f0ea9300aa3c8eda35eabde5
Malak Alaa <malakalaa1729@gmail.com>
2026-08-21 22:22:34 +0300

    Add risky Terraform configuration for testing

---
+++ terraform/risky.tf
@@ -0,0 +1,10 @@
+﻿resource "aws_db_instance" "risky" {
+  engine         = "mysql"
+  instance_class = "db.t3.micro"
+  name           = "risky_db"
+  username       = "admin"
+  password       = "hardcoded_password"
+  publicly_accessible = true
+  backup_retention_period = 0
+  storage_encrypted = false
+}
